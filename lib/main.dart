409d821be8ce6fc6a8058c611ea7d82d1c4a84bb
jinhyeon-dev <jinhyeon.dev@gmail.com>
2025-03-18 18:01:44 +0900

    chore: 환경변수처리

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gitmago/app.dart';
 import 'package:gitmago/providers/navigation_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  // .env 파일 로드
+  await dotenv.load(fileName: ".env");
+
   runApp(
     MultiProvider(
       providers: [
