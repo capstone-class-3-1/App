27c2669f5c72d5093c24f474699b4f20b0329b90
jinhyeon-dev <jinhyeon.dev@gmail.com>
2025-03-19 14:15:39 +0900

    chore: tapview 수정

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gitmago/theme/colors.dart';
-import 'package:gitmago/features/community/widgets/tab_bar_delegate.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -82,43 +81,14 @@ class HomePage extends StatelessWidget {
                   child: SizedBox(),
                 ),
               ),
-              SliverPersistentHeader(
-                delegate: TabBarDelegate(
-                  const TabBar(
-                    tabAlignment: TabAlignment.start,
-                    padding: EdgeInsets.only(left: 10.0),
-                    labelColor: AppColors.textColor2,
-                    unselectedLabelColor: AppColors.textColor2,
-                    indicatorSize: TabBarIndicatorSize.label,
-                    indicatorColor: AppColors.textColor2, // 선택된 탭 하단 표시 색상 변경
-                    isScrollable: true, // 탭이 많을 때 스크롤 가능하도록 설정
-                    labelPadding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ), // 탭 텍스트 사이의 여백 줄임
-                    tabs: [Tab(text: '추천'), Tab(text: '랭킹'), Tab(text: '모임')],
-                  ),
-                ),
-                pinned: true,
-              ),
             ];
           },
-          body: TabBarView(
-            children: [
-              // 추천 탭 내용
-              Container(color: Colors.white),
-              // 랭킹 탭 내용
-              ListView.builder(
-                itemCount: 20,
-                itemBuilder:
-                    (context, index) => ListTile(title: Text('랭킹 아이템 $index')),
-              ),
-              // 세일 탭 내용
-              ListView.builder(
-                itemCount: 20,
-                itemBuilder:
-                    (context, index) => ListTile(title: Text('세일 아이템 $index')),
-              ),
-            ],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(height: 100, color: AppColors.secondaryColor),
+              ],
+            ),
           ),
         ),
       ),
