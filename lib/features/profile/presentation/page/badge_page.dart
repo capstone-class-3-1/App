bc41cc858d906224f2aad1731b1f8d5759f4550d
jinhyeon-dev <jinhyeon.dev@gmail.com>
2025-03-23 23:14:10 +0900

    chore: badge page 추가

---
+++ lib/features/profile/presentation/page/badge_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BadgePage extends StatelessWidget {
+  const BadgePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Badge Page')));
+  }
+}
